3d46ff227d228294bdf0722832a400d4bf5500f4
otto18 <otto@ceramic.global>
2024-02-24 15:11:26 -0500

    updates to buy reducer in coffeesSlice.js

--- src/features/coffeesSlice.js
+++ src/features/coffeesSlice.js
@@ -1,80 +1,83 @@
 import { createSlice } from "@reduxjs/toolkit";
 //did i do the type properly?
-/** @type {{coffees: number, coffees/second: number, coffeeProducers: coffeeProducer[{name: string, quantity: number, coffee_second: number, cost: number, unlocked: boolean}]}} */
+/** @type {{coffees: number, coffees/second: number, coffeeProducers: coffeeProducer[{name: string, quantity: number, coffees_second: number, cost: number, unlocked: boolean}]}} */
 const initialState = {
   coffees: 0,
-  coffees_second: 1*(state.coffeeProducers.reduce((acc, coffeeProducer)=>{
-    acc + (coffeeProducer.quantity*coffeeProducer.coffee_second)
-    return acc
-  }),1),
+  coffees_second:
+    1 *
+    (state.coffeeProducers.reduce((acc, coffeeProducer) => {
+      acc + coffeeProducer.quantity * coffeeProducer.coffees_second;
+      return acc;
+    }),
+    1),
   coffeeProducers: [
     {
       name: "Chemex",
       quantity: 0,
-      coffee_second: 1,
+      coffees_second: 1,
       cost: 10,
       unlocked: false,
     },
     {
       name: "French Press",
       quantity: 0,
-      coffee_second: 2,
+      coffees_second: 2,
       cost: 50,
       unlocked: false,
     },
     {
       name: "Mr. Coffee",
       quantity: 0,
-      coffee_second: 5,
+      coffees_second: 5,
       cost: 100,
       unlocked: false,
     },
     {
       name: "Ten Cup Urn",
       quantity: 0,
-      coffee_second: 10,
+      coffees_second: 10,
       cost: 150,
       unlocked: false,
     },
     {
       name: "Espresso Machine",
       quantity: 0,
-      coffee_second: 20,
+      coffees_second: 20,
       cost: 250,
       unlocked: false,
     },
     {
       name: "Ten Gallon Urn",
       quantity: 0,
-      coffee_second: 50,
+      coffees_second: 50,
       cost: 500,
       unlocked: false,
     },
     {
       name: "Coffeeshop",
       quantity: 0,
-      coffee_second: 75,
+      coffees_second: 75,
       cost: 750,
       unlocked: false,
     },
     {
       name: "Coffee Factory",
       quantity: 0,
-      coffee_second: 100,
+      coffees_second: 100,
       cost: 1000,
       unlocked: false,
     },
     {
       name: "Coffee Fountain",
       quantity: 0,
-      coffee_second: 200,
+      coffees_second: 200,
       cost: 2500,
       unlocked: false,
     },
     {
       name: "Coffee River",
       quantity: 0,
-      coffee_second: 1,
+      coffees_second: 1,
       cost: 5000,
       unlocked: false,
     },
@@ -93,6 +96,8 @@ const coffeesSlice = createSlice({
     },
     /**the reducer below is not right -- i think this means i must pass i in when i map, not sure... */
     buy: (state, i) => {
+      state.coffees_second += state.coffeeProducers[i].coffees_second;
+      state.coffees -= state.coffeeProducers[i].cost;
       state.coffeeProducers[i].quantity += 1;
       state.coffeeProducers[i].cost *= 1.25;
     },
